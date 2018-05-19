ab9b514ab8cb778eb4cc58c953cda647e4a0d1e4
BRILYAN\USER <brilyan.andi@gmail.com>
2018-05-20 01:51:37 +0700

    Splashscreen baru

--- MyLaundry2/app/src/main/java/com/example/aqi/mylaundry2/SplashScreen.java
+++ MyLaundry2/app/src/main/java/com/example/aqi/mylaundry2/SplashScreen.java
@@ -5,27 +5,44 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
-
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 
 public class SplashScreen extends AppCompatActivity {
-
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
 
         //menghilangkan ActionBar
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_splashscreen);
-
-        final Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
+        imageView = (ImageView) findViewById(R.id.logo);
+        Animation myanim = AnimationUtils.loadAnimation(this, R.anim.splashscreen);
+        imageView.startAnimation(myanim);
+        final Intent i= new Intent(this,LoginActivity.class);
+        Thread timer = new Thread() {
             @Override
             public void run() {
-                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
-                finish();
+
+                try {
+                    sleep(5000);
+                } catch (
+                        InterruptedException e)
+
+                {
+                    e.printStackTrace();
+                } finally {
+                    startActivity(i);
+                    finish();
+                }
             }
-        }, 3000L); //3000 L = 3 detik
+        };
+        timer.start();
     }
+
 }
