2863bf32c33e03e5b4c88772eaf784ed5e5b7961
BRILYAN\USER <brilyan.andi@gmail.com>
2018-05-21 00:09:36 +0700

    info aplikasi (fixed)

--- MyLaundry2/app/src/main/java/com/example/aqi/mylaundry2/InfoAplikasiActivity.java
+++ MyLaundry2/app/src/main/java/com/example/aqi/mylaundry2/InfoAplikasiActivity.java
@@ -12,26 +12,9 @@ public class InfoAplikasiActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_aplikasi);
 
-        //Menambahkan tombol back pada appbar
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == android.R.id.home){
-            Intent intent = new Intent(InfoAplikasiActivity.this, BantuanActivity.class);
-            startActivity(intent);
-            finish();
-        }
-        return super.onOptionsItemSelected(item);
-    }
 
-    @Override
-    public void onBackPressed() {
-        Intent intent = new Intent(InfoAplikasiActivity.this, BantuanActivity.class);
-        startActivity(intent);
-        finish();
-    }
+
 }
