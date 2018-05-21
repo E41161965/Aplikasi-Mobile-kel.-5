5bcd8b6cd3394272514a798036b76d08cb0eb30f
BRILYAN\USER <brilyan.andi@gmail.com>
2018-05-21 23:06:17 +0700

    menambah button pada activity hubungi kami, menambahkan link untuk menghubungi lewat whatsapp dan instagram (untuk gmail masih belum,tampilan belum fix)

--- MyLaundry2/app/src/main/java/com/example/aqi/mylaundry2/HubungiKamiActivity.java
+++ MyLaundry2/app/src/main/java/com/example/aqi/mylaundry2/HubungiKamiActivity.java
@@ -1,9 +1,12 @@
 package com.example.aqi.mylaundry2;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class HubungiKamiActivity extends AppCompatActivity {
 
@@ -15,6 +18,8 @@ public class HubungiKamiActivity extends AppCompatActivity {
         //Menambahkan tombol back pada appbar
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
     }
 
     @Override
@@ -33,5 +38,50 @@ public class HubungiKamiActivity extends AppCompatActivity {
         Intent intent = new Intent(HubungiKamiActivity.this, BantuanActivity.class);
         startActivity(intent);
         finish();
+    }
+
+    public void whatsapp(View view) {
+        Button whatsapp;
+        whatsapp =(Button)findViewById(R.id.whatsapp);
+        whatsapp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent();
+                i.setAction(Intent.ACTION_VIEW);
+                i.addCategory(Intent.CATEGORY_BROWSABLE);
+                i.setData(Uri.parse("https://api.whatsapp.com/send?phone=6285812110912&text=Halo%20Admin"));
+                startActivity(i);
+            }
+        });
+    }
+
+    public void instagram(View view) {
+        Button instagram;
+        instagram =(Button)findViewById(R.id.instagram);
+        instagram.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent();
+                i.setAction(Intent.ACTION_VIEW);
+                i.addCategory(Intent.CATEGORY_BROWSABLE);
+                i.setData(Uri.parse("https://www.instagram.com/e_laundryjember/"));
+                startActivity(i);
+            }
+        });
+
+    }
+    public void gmail(View view) {
+        Button gmail;
+        gmail =(Button)findViewById(R.id.gmail);
+        gmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent();
+                i.setAction(Intent.ACTION_VIEW);
+                i.addCategory(Intent.CATEGORY_BROWSABLE);
+                i.setData(Uri.parse("https://www.instagram.com/e_laundryjember/"));
+                startActivity(i);
+            }
+        });
     }
 }
