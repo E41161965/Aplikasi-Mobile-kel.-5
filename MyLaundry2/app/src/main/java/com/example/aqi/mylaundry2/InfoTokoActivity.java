aaaf66a78bc0452f5ad704077c747e11c1ace810
BRILYAN\USER <brilyan.andi@gmail.com>
2018-05-26 01:54:53 +0700

    Tampilan sementara Info Toko beserta contact person yang bisa di klik dan terhubung dengan aplikasi telpon

--- MyLaundry2/app/src/main/java/com/example/aqi/mylaundry2/InfoTokoActivity.java
+++ MyLaundry2/app/src/main/java/com/example/aqi/mylaundry2/InfoTokoActivity.java
@@ -1,16 +1,25 @@
 package com.example.aqi.mylaundry2;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.util.Linkify;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class InfoTokoActivity extends AppCompatActivity {
-
+private TextView tv1,tv2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_toko);
+        tv1 = (TextView) findViewById(R.id.tv1);
+        tv2 = (TextView) findViewById(R.id.tv2);
+        tv1.setLinkTextColor(Color.parseColor("#ffffffff"));
+        tv2.setLinkTextColor(Color.parseColor("#ffffffff"));
+        Linkify.addLinks(tv1,Linkify.ALL);
+        Linkify.addLinks(tv2,Linkify.ALL);
 
         //Menambahkan tombol back pada appbar
         getSupportActionBar().setDisplayShowHomeEnabled(true);
--- MyLaundry2/app/src/main/java/com/example/aqi/mylaundry2/NotakuActivity.java
+++ MyLaundry2/app/src/main/java/com/example/aqi/mylaundry2/NotakuActivity.java
@@ -5,7 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 
-public class NotakuActivity extends AppCompatActivity {
+public class NotakuActivity extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
