ad59bc79e034566ea182c7564ce1234cd6698ee3
BRILYAN\USER <brilyan.andi@gmail.com>
2018-06-06 00:37:33 +0700

    mlm

--- MyLaundry2/app/src/main/java/com/example/aqi/mylaundry2/DetailTransaksiActivity.java
+++ MyLaundry2/app/src/main/java/com/example/aqi/mylaundry2/DetailTransaksiActivity.java
@@ -5,10 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 
 public class DetailTransaksiActivity extends AppCompatActivity {
-    private Button edit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
